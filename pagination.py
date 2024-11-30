c026847fa6c44271dee8c33a24738c4bf616531d
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2024-11-30 21:34:12 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-homepage = st.Page("pages/homepage.py", title="Welcome", icon=":material/home:")
-new_user_page = st.Page("pages/new_users.py", title="New Users", icon=":material/add_circle:")
-team_page = st.Page("pages/the_team.py", title="The team", icon=":material/groups:")
-verify_page = st.Page("pages/verify_users.py", title="User Verification", icon=":material/verified_user:")
+homepage = st.Page("webpages/homepage.py", title="Welcome", icon=":material/home:")
+new_user_page = st.Page("webpages/new_users.py", title="New Users", icon=":material/add_circle:")
+team_page = st.Page("webpages/the_team.py", title="The team", icon=":material/groups:")
+verify_page = st.Page("webpages/verify_users.py", title="User Verification", icon=":material/verified_user:")
 
 pg = st.navigation([homepage, new_user_page, verify_page, team_page])
 
